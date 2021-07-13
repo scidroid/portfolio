21df709a62a999662b1081c7e48d7361ccb1ec75
SciDroid <scidroidgames@gmail.com>
2021-07-13 12:27:11 -0500

    Add files via upload

---
+++ ram/index.js
@@ -0,0 +1,16 @@
+fetch("https://rickandmortyapi.com/api/character/")
+  .then((res) => res.json())
+  .then((res) => {
+    for (let index = 0; index < 20; index++) {
+      document.querySelector("#container").insertAdjacentHTML(
+        "beforeend",
+        `
+          <article>
+            <img src="${res.results[index].image}">
+            <h3>${res.results[index].name}</h3>
+            <p>${res.results[index].origin.name}</p>
+          </article>
+        `
+      );
+    }
+  });
